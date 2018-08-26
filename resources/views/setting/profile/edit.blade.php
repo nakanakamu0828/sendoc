c8783d41dfcdd75b147c4502aabf1d80b25757bc
Yuki Nakamura <nakamura.yuuki.0606@gmail.com>
2018-08-26 21:06:31 +0900

    refactor: レイアウト調整

--- resources/views/setting/profile/edit.blade.php
+++ resources/views/setting/profile/edit.blade.php
@@ -18,8 +18,8 @@
                         @include('layouts.messages')
 
                         <div class="m-b-20" style="padding-left: 35%">
-                            <figure class="image is-circle is-128x128">
-                                <img src="https://bulma.io/images/placeholders/128x128.png">
+                            <figure class="image is-128x128">
+                                <img class="is-rounded" src="https://bulma.io/images/placeholders/128x128.png">
                             </figure>
                         </div>
 
